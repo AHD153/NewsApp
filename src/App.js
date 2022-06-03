3f50d739991023cd24dd7259f302c246da33809e
Ayush Dodiya <ayushdodiya153@gmail.com>
2022-06-03 15:49:57 +0530

    Class Based Component
    This is the website made using class based components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Navbar from './Components/Navbar';
+import React, { Component } from 'react'
+import News from './Components/News';
+import LoadingBar from 'react-top-loading-bar'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+
+export class App extends Component {
+
+  pageSize = 6;
+
+  // This technique is used to get apiKey from the .env.local file
+  // Due to this apiKey is not going to be displayed to anyone.
+  apiKey = process.env.REACT_APP_NEWS_API;
+
+  state = {
+    progress:0
+  }
+
+  setProgress = (progress) => {
+    this.setState({progress: progress})
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <LoadingBar
+            color='#f11946'
+            // progress={50}
+            progress={this.state.progress}
+          />
+          <Navbar />
+
+
+
+          <Routes>
+            <Route exact path="/" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} category="general" country="in" />} />
+            <Route exact path="/business" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize={this.pageSize} category="business" country="in" />} />
+            <Route exact path="/entertainment" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={this.pageSize} category="entertainment" country="in" />} />
+            <Route exact path="/general" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} category="general" country="in" />} />
+            <Route exact path="/health" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={this.pageSize} category="health" country="in" />} />
+            <Route exact path="/science" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={this.pageSize} category="science" country="in" />} />
+            <Route exact path="/sports" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={this.pageSize} category="sports" country="in" />} />
+            <Route exact path="/technology" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize={this.pageSize} category="technology" country="in" />} />
+          </Routes>
+
+        </Router>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
